fcd70bd957e67ebd0c900e563577edefad74c042
Mehul Nandaniya <220130107062@gecg28.ac.in>
2025-07-22 10:56:09 +0530

    feat: add Art component and implement GSAP animations for enhanced visual effects

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -1,96 +1,67 @@
-import React from 'react';
-import { cocktailLists, mockTailLists } from '../constants/index.js';
-import { useGSAP } from '@gsap/react';
-import gsap from 'gsap';4
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap';
+import { cocktailLists, mockTailLists } from '../constants/index'
 
-gsap.registerPlugin(ScrollTrigger);
-
-function Cocktails() {
-
-   useGSAP(() => {
-  const parallaxTimeline = gsap.timeline({
-    scrollTrigger: {
-      trigger: "#cocktails",
-      start: "top 60%",
-      end: "bottom top",
-      scrub: true,
-    },
-  });
-
-  parallaxTimeline
-    .from("#c-left-leaf", {
-      x: -150,
-      y: 150,
-      rotate: -15,
-      opacity: 0,
-      scale: 0.8,
-      ease: "power2.out",
-    })
-    .from(
-      "#c-right-leaf",
-      {
-        x: 150,
-        y: 150,
-        rotate: 15,
-        opacity: 0,
-        scale: 0.8,
-        ease: "power2.out",
-      },
-      "<" // start at same time as previous
-    );
-});
-
-
-  return (
-    <section id='cocktails' className='noisy'>
-
-      <img src="/images/cocktail-left-leaf.png" id='c-left-leaf' alt="l-leaf" />
-      <img src="/images/cocktail-right-leaf.png" id='c-right-leaf' alt="r-leaf" />
-
-      <div className='list'>
-
-        <div className='popular'>
-          <h2>Most Popular Cocktails</h2>
-
-          <ul>
-            {cocktailLists.map(({ name, country, detail, price }) => (
-              <li key={name}>
-                <div className='md:me-28'>
-                  <h3>{name}</h3>
-                  <p>{country} | {detail}</p>
-                </div>
-                <span>- {price}</span>
-              </li>
-            ))}
-          </ul>
-
-        </div>
-        
-        <div className='loved'>
-          <h2>Most Loved Mocktails</h2>
-
-          <ul>
-            {mockTailLists.map(({ name, country, detail, price }) => (
-              <li key={name}>
-                <div className='me-28'>
-                  <h3>{name}</h3>
-                  <p>{country} | {detail}</p>
-                </div>
-                <span>- {price}</span>
-              </li>
-            ))}
-          </ul>
-
-        </div>
-
-
-
-      </div>
-
-       
-    </section>
-  );
+const Cocktails = () => {
+ useGSAP(() => {
+	const parallaxTimeline = gsap.timeline({
+	 scrollTrigger: {
+		trigger: '#cocktails',
+		start: 'top 30%',
+		end: 'bottom 80%',
+		scrub: true,
+	 }
+	})
+	
+	parallaxTimeline
+	 .from('#c-left-leaf', {
+		x: -100, y: 100
+	})
+	 .from('#c-right-leaf', {
+		x: 100, y: 100
+	})
+ })
+ 
+ return (
+	<section id="cocktails" className="noisy">
+	 <img src="/images/cocktail-left-leaf.png" alt="l-leaf" id="c-left-leaf" />
+	 <img src="/images/cocktail-right-leaf.png" alt="r-leaf" id="c-right-leaf" />
+	 
+	 <div className="list">
+		<div className="popular">
+		 <h2>Most popular cocktails:</h2>
+		 
+		 <ul>
+			{cocktailLists.map(({ name, country, detail, price }) => (
+			 <li key={name}>
+				<div className="md:me-28">
+				 <h3>{name}</h3>
+				 <p>{country} | {detail}</p>
+				</div>
+				<span>- {price}</span>
+			 </li>
+			))}
+		 </ul>
+		</div>
+		
+		<div className="loved">
+		 <h2>Most loved mocktails:</h2>
+		 
+		 <ul>
+			{mockTailLists.map(({ name, country, detail, price }) => (
+			 <li key={name}>
+				<div className="me-28">
+				 <h3>{name}</h3>
+				 <p>{country} | {detail}</p>
+				</div>
+				<span>- {price}</span>
+			 </li>
+			))}
+		 </ul>
+		</div>
+	 </div>
+	</section>
+ )
 }
 
-export default Cocktails;
+export default Cocktails
