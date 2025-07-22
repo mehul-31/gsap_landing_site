b981ddbc80e7bbb953d26e0dffa84a677228636e
Mehul Nandaniya <220130107062@gecg28.ac.in>
2025-07-22 09:35:59 +0530

    feat: add Cocktails component and restructure constants; remove unused index.js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {navLinks} from "../../constants/index.js";
+import {navLinks} from "../constants/index.js";
 import {useGSAP} from "@gsap/react";
 import gsap from "gsap";
 function Navbar() {
